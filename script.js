5316929e7951138bab32333ce5cdb5b6ce1a0762
aliyasmuhammad12 <aliyasmuhammad715125@gmail.com>
2024-03-22 13:10:53 +0500

    second commit

--- script.js
+++ script.js
@@ -8,32 +8,31 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbols = "@#$%*";
 
-const allChracters = upperCase + lowerCase + number +symbols;
+const allChracters = upperCase + lowerCase + number + symbols;
 
-function createPassword(){
-    let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
-    password += symbols[Math.floor(Math.random() * symbols.length)];
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
 
-    while(length > password.length){
-        password += allChracters[Math.floor(Math.random() * allChracters.length)];
-
-    }
-    passwordBox.value = password;
+  while (length > password.length) {
+    password += allChracters[Math.floor(Math.random() * allChracters.length)];
+  }
+  passwordBox.value = password;
 }
 
-button.onclick = function(){
-    createPassword();
-    console.log("clicked")
+button.onclick = function () {
+  createPassword();
+  console.log("clicked");
+};
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
 }
 
-function copyPassword(){
-    passwordBox.select();
-    document.execCommand("copy");
-}
-
-copy.onclick = function(){
-    copyPassword();
-}
+copy.onclick = function () {
+  copyPassword();
+};
